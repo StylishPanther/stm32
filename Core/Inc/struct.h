981bdffe8a44c5ac06e623bc03f173805e4e712a
StylishPanther <zazz0907@naver.com>
2022-08-27 00:06:46 +0900

    [jinho] add the struct header and modify gitignore

---
+++ Core/Inc/struct.h
@@ -0,0 +1,28 @@
+//###########################################################################
+//
+// FILE		: struct.h
+//
+// TITLE	: struct.h file.
+//
+// Author	: Lee Jin Ho
+//
+// Company	: Maze & Hz
+//
+//###########################################################################
+// $Release Date: 2022.08.26 $
+//###########################################################################
+
+
+#ifdef __MAIN__
+	#ifndef __STRUCT_EXT__
+		#define __STRUCT_EXT__
+	#endif
+#else
+	#ifndef __STRUCT_EXT__
+		#define __STRUCT_EXT__	extern
+	#endif
+#endif	
+	
+
+
+		
